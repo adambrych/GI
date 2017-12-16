c68053402d4ceab79b715aae85ae3f3671067273
Marcin Halczuk <halczukmarcin@gmail.com>
2017-12-16 21:08:21 +0100

    Add human heat map reader

--- Config.py
+++ Config.py
@@ -4,3 +4,4 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = ROOT_DIR + '\\Data'
 PLOTS_PATH = ROOT_DIR + '\\Plots'
+IMG_PATH = ROOT_DIR + '\\Img'
---
+++ Readers/SVG_Body_Reader.py
@@ -0,0 +1,53 @@
+import xml.dom.minidom as dom
+import Config
+import time
+
+
+def timestamp():
+    return str(int(round(time.time() * 1000)))
+
+
+def read_svg():
+    doc = dom.parse(Config.IMG_PATH + "\\human_body.svg")
+    paths = doc.getElementsByTagName("path")
+    body_parts = dict()
+    body_parts["head"] = []
+    body_parts["neck"] = []
+    body_parts["arms"] = []
+    body_parts["legs"] = []
+    body_parts["chest"] = []
+    for element in paths:
+        id = element.getAttribute("id")
+        if id == "head":
+            body_parts["head"].append(element)
+        elif id == "neck":
+            body_parts["neck"].append(element)
+        elif id.find("arm") != -1:
+            body_parts["arms"].append(element)
+        elif id.find("Leg") != -1:
+            body_parts["legs"].append(element)
+        elif id.find("chest") != -1:
+            body_parts["chest"].append(element)
+    return doc, body_parts
+
+
+def set_color(body_parts, part, color):
+    element_array = body_parts[part]
+    for element in element_array:
+        style_string = element.getAttribute("style")
+        style_string = style_string[12:]
+        element.setAttribute("style", "fill:" + color + style_string)
+
+
+def save_svg(doc):
+    filename = "injury_head_map_"+timestamp()+".svg"
+    with open(Config.IMG_PATH + "\\" + filename, "w") as svg_file:
+        doc.writexml(svg_file)
+    return filename
+
+
+doc, body_parts = read_svg()
+set_color(body_parts, "chest", "#00fff2")
+save_svg(doc)
+
+
